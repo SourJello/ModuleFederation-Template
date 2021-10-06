b44e8f4a52ae0095defcbb78ce458b2274e72ab2
Patrick K <patrick.kitchens1@gmail.com>
2021-10-05 21:54:38 -0400

    Installed react and react dom to home
    added skeleton apps to home common ui and admin

---
+++ Home/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+    <div>
+      <div>Hi there, I'm React from React.</div>
+    </div>
+  );
+  
+  ReactDOM.render(<App />, document.getElementById("app"));
